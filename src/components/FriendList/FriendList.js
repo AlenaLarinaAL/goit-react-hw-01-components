b4b1be04e161bdbf7ee4fd13615a32a21963f892
AlenaLarinaAL <alenalarina605@gmail.com>
2023-07-03 15:56:37 +0000

    add

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,3 +1,5 @@
+
+
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import css from './FriendList.module.css';
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -7,7 +7,7 @@ export const Statistics = ({ items }) => {
       {items.map((item, index) => (
         <li className={`${css.item} ${css[`item-${index + 1}`]}`} key={item.id}>
           <span className={css.label}>{item.label}</span>
-          <span className={css.percentage}>{item.percentage}</span>
+          <span className={css.percentage}>{item.percentage}%</span>
         </li>
       ))}
     </ul>
@@ -24,25 +24,3 @@ Statistics.propTypes = {
   ).isRequired,
 };
 
-// export const Statistics = ({ id, label, percentage }) => {
-//   return (
-//     <ul class="stat-list">
-//       <li class="item">
-//         <span class="label">.docx</span>
-//         <span class="percentage">4%</span>
-//       </li>
-//       <li class="item">
-//         <span class="label">.mp3</span>
-//         <span class="percentage">14%</span>
-//       </li>
-//       <li class="item">
-//         <span class="label">.pdf</span>
-//         <span class="percentage">41%</span>
-//       </li>
-//       <li class="item">
-//         <span class="label">.mp4</span>
-//         <span class="percentage">12%</span>
-//       </li>
-//     </ul>
-//   );
-// };
